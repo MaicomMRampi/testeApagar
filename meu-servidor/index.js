0ce1d985e7dfaba02f3e54e98e023b777fe4ddd7
Maicom <maicom.azus@gmail.com>
2024-10-17 15:01:34 -0300

    testsn cors

--- meu-servidor/index.js
+++ meu-servidor/index.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors'); // Importando o CORS
 const userRoutes = require('./routes/userRoutes');
+
 const app = express();
+
+// Habilitando o CORS para todas as origens
+app.use(cors()); // Permite todas as origens
 
 app.use(express.json()); // Para parsear JSON
 app.use('/users', userRoutes);
